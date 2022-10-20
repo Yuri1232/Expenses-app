b766535ca6f2f0c8807137b5cb96fcd4d62a07e8
Yuri1232 <younisyounis12@gmail.com>
2022-10-20 18:46:08 +0300

    d

---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,15 @@
+ function ExpenseItems()
+ {
+    return(
+        <div>
+         <div>Janury 7th 1995</div>
+         <div>
+            <h1>Car insurence</h1>
+            <div>$299</div>
+         </div>
+        </div>
+    )
+
+ }
+
+ export default ExpenseItems
